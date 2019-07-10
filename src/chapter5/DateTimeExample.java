e6a2cdadcb4d4541b4b2808c136fd483ba6c6ba8
Lucas Daniel Borges de Oliveira <lukys_taylor@hotmail.com>
2019-07-10 16:05:31 -0300

    adds instant, period and duration

--- src/chapter5/DateTimeExample.java
+++ src/chapter5/DateTimeExample.java
@@ -1,9 +1,8 @@
 package chapter5;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 
 public class DateTimeExample {
 }
@@ -17,5 +16,58 @@ class DateTimeZone {
         ZonedDateTime zoned = ZonedDateTime.of(LocalDate.now(), LocalTime.now(), zone);
         System.out.println(zoned);
 
+    }
+}
+
+/**
+ * Period
+ * Can deal with data not time
+ */
+class PeriodExample {
+
+    public static void main(String args[]) {
+        LocalDate start = LocalDate.of(2015, Month.JANUARY, 1);
+        Period period = Period.ofMonths(1);
+
+        System.out.println(start.plus(period));
+    }
+}
+
+/**
+ * Duration
+ * Can deal with time
+ */
+class DurationExample {
+
+    public static void main(String args[]) {
+        LocalDate date  = LocalDate.of(2019, 01, 10);
+        LocalTime time = LocalTime.of(16, 15, 0);
+        LocalDateTime dateTime = LocalDateTime.of(date, time);
+        Duration duration = Duration.ofHours(20);
+        dateTime = dateTime.plus(duration);
+        System.out.println("modified  duration " + dateTime);
+
+    }
+}
+
+/**
+ * Instant
+ * Instant is based in Zone so we is only compatible with ZoneDataTime
+ * So the instant is based in GMT time
+ * We can deal with day or smaller
+ * Day, Hour, Minute, seconds ...
+ */
+class InstantExample {
+
+    public static void main(String args[]) {
+        LocalDate date  = LocalDate.of(2019, 01, 10);
+        LocalTime time = LocalTime.of(16, 15, 0);
+        ZoneId zone =  ZoneId.of("America/Sao_Paulo");
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(date, time, zone);
+        System.out.println("Zone date " + zonedDateTime);
+        Instant instant = zonedDateTime.toInstant();
+        System.out.println("Instante date " + instant);
+        instant = instant.plus(2, ChronoUnit.DAYS);
+        System.out.println("Instant date modified " + ZonedDateTime.ofInstant(instant, zone));
     }
 }
