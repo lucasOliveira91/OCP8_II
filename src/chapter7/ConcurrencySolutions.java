13e85d8f3afaab9ad79990c9b849acfd54751f3b
Lucas Daniel <ldoliveira91@outlook.com>
2019-07-24 19:34:10 -0300

    addas fork and join framworks examples

--- src/chapter7/ConcurrencySolutions.java
+++ src/chapter7/ConcurrencySolutions.java
@@ -1,8 +1,12 @@
 package chapter7;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Queue;
+import java.util.Random;
 import java.util.concurrent.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ConcurrencySolutions {
 
@@ -57,5 +61,64 @@ class BlockingQueue {
         for (String item: queue) {
             System.out.println(item);
         }
+    }
+}
+
+class Reduce {
+
+    public static void main(String[] args) {
+        Stream<String> stream = Arrays.asList("L","U","C","A","S").parallelStream();
+
+        String reduce = stream.reduce("", String::concat);
+        System.out.println(reduce);
+
+        Stream<String> stream2 = Arrays.asList("L","U","C","A","S").parallelStream();
+        List<String> list = stream2.collect(Collectors.toList());
+        System.out.println(list);
+
+    }
+}
+
+class ForkJoinFramework {
+
+    static class WeighAnimalAction extends RecursiveAction{
+
+        private int start;
+        private int end;
+        private Double[] weights;
+
+        public WeighAnimalAction(Double[] weights, int start, int end ){
+            this.weights = weights;
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        protected void compute() {
+            if(end - start <= 3) {
+                for (int i = start; i < end; i++) {
+                    weights[i] = (double) new Random().nextInt(100);
+                    System.out.println("Animal weighed: " + i);
+                }
+            }else {
+                int middle = start + ((end-start) / 2);
+                System.out.println("[start="+start+",middle=" + middle + ", end=" +end + "]");
+                invokeAll(new WeighAnimalAction(weights, start, end),
+                          new WeighAnimalAction(weights, start, end)
+                );
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Double[] weights = new Double[10];
+        ForkJoinTask<?> task = new WeighAnimalAction(weights,0, weights.length);
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.invoke(task);
+
+        System.out.println();
+        System.out.print("Weights: ");
+        Arrays.asList(weights).stream().forEach(
+                d -> System.out.print(d.intValue()+" "));
     }
 }
