993e9a9da37a99ab5181862751d6ff6ca05e3dac
Lucas Oliveira <ldoliveira91@outlook.com>
2019-08-07 22:46:06 -0300

    resolving exercicies

--- src/chapter9/PathExample.java
+++ src/chapter9/PathExample.java
@@ -1,13 +1,17 @@
 package chapter9;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class PathExample {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Path path = Paths.get("C:\\teste\\teste1\\teste2");
         System.out.println(path.subpath(1,3).getName(1).toAbsolutePath());
         System.out.println(path.toAbsolutePath());
+
+        Files.walk(path).filter((p) -> p.isAbsolute());
     }
 }
