ce7f32b750632c0199e4529fd70641732472bdde
Lucas Daniel Borges de Oliveira <lucas.oliveira@postalis.org.br>
2019-08-16 14:46:43 -0300

    adds exercicies

--- src/Exam/SimulatorForths.java
+++ src/Exam/SimulatorForths.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.sql.*;
 
 public class SimulatorForths {
     public static void main(String[] args) throws IOException {
@@ -17,3 +18,18 @@ public class SimulatorForths {
         Files.find(path, 1, (p, a) -> a.isRegularFile()).forEach(System.out::println);
     }
 }
+
+class QuestionFortyOne {
+    public static void main(String[] args) throws SQLException {
+        Connection conn = DriverManager.getConnection("dbURL", "userName", "passWord");
+        String query = "SELECT id FROM Employee";
+        try (Statement stmt = conn.createStatement()) {
+            ResultSet rs = stmt.executeQuery(query);
+            stmt.executeQuery("SELECT id FROM Customer");
+            while (rs.next()) {
+                //process the results
+                System.out.println("Employee ID: "+ rs.getInt("id"));
+            }
+        } catch (Exception e) {
+        }
+    }
