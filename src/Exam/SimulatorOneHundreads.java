5b53433e911b626f16e9b9b40ebb8361cec9f8c2
Lucas Daniel Borges de Oliveira <lucas.oliveira@postalis.org.br>
2019-08-22 09:44:05 -0300

    adds more answers

--- src/Exam/SimulatorOneHundreads.java
+++ src/Exam/SimulatorOneHundreads.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -15,7 +17,7 @@ public class SimulatorOneHundreads {
 class QuestionTwelve {
     public static void main(String[] args) throws IOException, InterruptedException {
         ExecutorService executor = Executors.newCachedThreadPool();
-        Stream<Path> listOfFiles = Files.walk(Paths.get("Java Projects"));
+        Stream<Path> listOfFiles = Files.walk(Paths.get("C://uploads"));
         listOfFiles.forEach(line -> {
             executor.execute(new FileThread(line.getFileName().toString())); //
 
@@ -28,6 +30,49 @@ class QuestionTwelve {
 
  class FileThread implements Runnable {
     String fName;
-    public FileThread(String fName) { this.fName = fName; }
-    public void run (){ System.out.println(fName);}
-}
+     public FileThread(String fName) { this.fName = fName; }
+     public void run (){
+         for (int i = 0; i < 100; i++) {
+             System.out.println("Executando arquivo : " + fName);
+         }
+     }
+ }
+
+ class QuestionThrteen {
+    interface Contract {}
+    static class Super implements Contract{};
+    static class Sub extends Super{}
+     public static void main(String[] args) {
+        List obj = new ArrayList();
+
+        Contract c1 = new Super();
+        Contract c2 = new Sub();
+        Super s1 = new Sub();
+
+        obj.add(c1);
+        obj.add(c2);
+        obj.add(s1);
+
+         for (Object o : obj) {
+             System.out.println(o.getClass().getName());
+         }
+     }
+ }
+
+ class QuestionOneHundreadFourteen {
+     public static void main(String[] args) {
+         int rateOfInterest = 0;
+         String accountType = "LOAN";
+         switch (accountType) {
+             case "RD":
+                 rateOfInterest = 5;
+                 break;
+             case "FD":
+                 rateOfInterest = 10;
+                 break;
+             default:
+                 assert false : "No interest for this account"; //line n1
+         }
+         System.out.println("Rate of interest:" + rateOfInterest);
+     }
+ }
