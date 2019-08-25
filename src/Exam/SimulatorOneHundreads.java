3c6bd258849cbfbf85056229d8068222c482c234
Lucas Oliveira <ldoliveira91@outlook.com>
2019-08-25 20:24:23 -0300

    adds

--- src/Exam/SimulatorOneHundreads.java
+++ src/Exam/SimulatorOneHundreads.java
@@ -1,5 +1,7 @@
 package Exam;
 
+import Exam.QuestionOneHundreadEighteen.Runner;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,10 +9,9 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -114,3 +115,78 @@ class QuestionTwelve {
         public Integer getEAge () {return eAge;}
         public String getEName () {return eName;}
 }
+
+class QuestionOneHundreadEighteen{
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService es = Executors.newFixedThreadPool(2);
+        Future f1 = es.submit (new Caller ("Call"));
+        Future f2 = es.submit (new Runner ("Run"));
+        String str1 = (String) f1.get();
+        String str2 = (String) f2.get();//line n1
+        System.out.println(str1+ ":" + str2);
+    }
+
+    static class Caller implements Callable<String> {
+        String str;
+        public Caller (String s) {this.str=s;}
+        public String call()throws Exception { return str.concat ("Caller");}
+    }
+    static class Runner implements Runnable {
+        String str;
+        public Runner (String s) {this.str=s;}
+        public void run () { System.out.println (str.concat ("Runner"));}
+    }
+}
+
+class OneHundredTwenty{
+    public static void main(String[] args) {
+        List<Integer> codes = Arrays.asList (10, 20);
+        UnaryOperator<Double> uo = s -> s + 10.0;
+//        codes.replaceAll(uo);
+//        codes.forEach(c -> System.out.println(c));
+        x(20.0);
+    }
+
+    static void  x (Double x) {
+
+    }
+}
+
+
+class OneHundredTwentyOne {
+    public static void main(String[] args) {
+
+    }
+
+    abstract class Shape {
+        Shape() {
+            System.out.println("Shape");
+        }
+
+        protected void area() {
+            System.out.println("Shape");
+        }
+    }
+
+    class Square extends Shape {
+        int side;
+//        Square(){}
+        Square(int side) {
+            /* insert code here */
+            this.side = side;
+        }
+
+        public void area ( ) { System.out.println ("Square"); }
+    }
+
+    class Rectangle extends Square {
+        int len, br;
+
+        Rectangle(int x, int y) {
+            super(x);
+            /* insert code here */
+            len = x; br = y;
+        }
+    }
+}
+
