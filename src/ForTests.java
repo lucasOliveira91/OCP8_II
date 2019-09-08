6efe87b52de8223c02d748ec96da9d4898414878
Lucas Oliveira <ldoliveira91@outlook.com>
2019-09-08 12:42:37 -0300

    adds

--- src/ForTests.java
+++ src/ForTests.java
@@ -1,13 +1,30 @@
 import training.HayStore;
 
+import java.util.Optional;
+
 public class ForTests {
     public static void main(String[] args) {
 
-        HayStore hay = HayStore.getInstance();
-        hay.addHay(1);
+        Address address = null;
+        Optional<Address> addrs1 = Optional.ofNullable (address);
 
     }
 
+    private static void x(Address o) {
 
+
+    }
+
+    class Address {
+        String city = "New York";
+
+        public String getCity() {
+            return city;
+        }
+
+        public String toString() {
+            return city;
+        }
+    }
 
 }
