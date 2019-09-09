f790277b070f7d3c593106eb28837b25d04a77c9
Lucas Daniel Borges de Oliveira <lucas.oliveira@postalis.org.br>
2019-09-09 17:22:44 -0300

    adicionando mais questoes

--- src/ForTests.java
+++ src/ForTests.java
@@ -3,28 +3,7 @@ import training.HayStore;
 import java.util.Optional;
 
 public class ForTests {
-    public static void main(String[] args) {
-
-        Address address = null;
-        Optional<Address> addrs1 = Optional.ofNullable (address);
+    public static void main(String[] args)  {
 
     }
-
-    private static void x(Address o) {
-
-
-    }
-
-    class Address {
-        String city = "New York";
-
-        public String getCity() {
-            return city;
-        }
-
-        public String toString() {
-            return city;
-        }
-    }
-
 }
