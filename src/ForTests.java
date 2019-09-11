a5176f3991405e0ebb4bbffcf8c058692060df0e
Lucas Daniel Borges de Oliveira <lucas.oliveira@postalis.org.br>
2019-09-11 09:39:31 -0300

    add

--- src/ForTests.java
+++ src/ForTests.java
@@ -1,13 +1,8 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
+import java.io.IOException;
 
 public class ForTests {
-    public static void main(String[] args)  {
-        List<Integer> list1 = Arrays.asList(10, 20);
-        List<Integer> list2 = Arrays.asList(15, 30);
+    public static void main(String[] args) throws IOException {
 
-        Stream.of(list1, list2).flatMap(l -> l.stream()).forEach(System.out::println);
     }
 }
 
