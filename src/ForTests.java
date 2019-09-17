273bd21a643c9a873346838824eecda23fec7741
Lucas Daniel Borges de Oliveira <lucas.oliveira@postalis.org.br>
2019-09-17 08:31:43 -0300

    adds

--- src/ForTests.java
+++ src/ForTests.java
@@ -1,7 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class ForTests {
     public static void main(String[] args)   {
 
     }
+
 
 
 }
