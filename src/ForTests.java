956813a25b7abdb0317e36e3f06103bbedf14198
Lucas Daniel Borges de Oliveira <lucas.oliveira@postalis.org.br>
2019-09-19 10:04:35 -0300

    adds

--- src/ForTests.java
+++ src/ForTests.java
@@ -1,19 +1,25 @@
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 
 public class ForTests {
-    public static void main(String[] args) {
-        List<Integer> nums = Arrays.asList (10, 20, 8);
-        System.out.println (
-               nums.stream().max(Comparator.comparing(Integer::intValue).reversed()).get()
+    public static class Test<T> {
+        private T t;
+        public T get () {
+            return t;
+        }
+        public void set (T t) {
+            this.t = t;
+        }
+        public static void main (String args [ ] ) {Test<String> type = new Test<>();
+            Test type1 = new Test (); //line n1
+            type.set("Java");
+            type1.set(new O()); //line n2
+            System.out.print(type.get() + " " + type1.get());
+        }
 
-
-        );
+        static class O {}
     }
-
 }
-
 
 
 
