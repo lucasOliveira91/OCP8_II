50bf728ed2999c18a341b44362691a7b6abe6041
Lucas Daniel Borges de Oliveira <lucas.oliveira@postalis.org.br>
2019-09-23 09:21:15 -0300

    adds

--- src/ForTests.java
+++ src/ForTests.java
@@ -1,32 +1,13 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ForTests {
-    static class Student {
-        String course, name, city;
-        public Student (String name, String course, String city) {
-            this.course = course; this.name = name; this.city = city;
-        }
-        public String toString() {
-            return course + ":" + name + ":" + city;
-        }
-
-        String getCourse() {
-            return this.course;
-        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        System.out.print ("Enter GDP: ");
+        
     }
-
-        public static void main(String[] args) {
-            List<Student> stds = Arrays.asList(
-                    new Student ("Jessy", "Java ME", "Chicago"),
-                    new Student ("Helen", "Java EE", "Houston"),
-                    new Student ("Mark", "Java ME", "Chicago"));
-            stds.stream()
-                    .collect(Collectors.groupingBy(Student::getCourse))
-                    .forEach((src, res) -> System.out.println(src + res));
-        }
 }
 
 
