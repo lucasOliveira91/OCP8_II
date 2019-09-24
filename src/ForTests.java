ffc03aa40b792e9638313d587d12ff839e74e71b
Lucas Daniel Borges de Oliveira <lucas.oliveira@postalis.org.br>
2019-09-24 11:09:00 -0300

    dass

--- src/ForTests.java
+++ src/ForTests.java
@@ -1,22 +1,10 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class ForTests {
 
-    static void doStuff() throws ArithmeticException, NumberFormatException, Exception {
-        if (Math.random() >-1) throw new Exception ("Try again");
-    }
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) {
-        try {
-            doStuff ();
-        } catch (ArithmeticException | NumberFormatException e) {
-            System.out.println (e.getMessage()); }
-        catch (Exception e) {
-            System.out.println (e.getMessage()); }
     }
-
 }
 
 
